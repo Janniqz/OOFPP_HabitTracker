dc211a9f154b0c4f886791ef1597e2cf823906d5
Jannik Weise <mail@jannikweise.de>
2023-09-13 22:11:00 +0200

    Added Command Stubs
    Added Command Stubs for both modules. The following Commands are currently planned:
    
    Analytics:
    - list
    - streak
    
    Habit:
    - create
    - delete
    - modify
    - complete

--- modules/analytics.py
+++ modules/analytics.py
@@ -1,7 +1,17 @@
 import click
-from click import Group
+from click import Command, Group
 
 
 @click.group()
 def analytics() -> Group:
+    pass
+
+
+@analytics.command(name='list')
+def analytics_list() -> Command:
+    pass
+
+
+@analytics.command(name='streak')
+def analytics_streak() -> Command:
     pass
--- modules/habit.py
+++ modules/habit.py
@@ -1,8 +1,27 @@
 import click
-from click import Group
+from click import Command, Group
 
 
 @click.group()
 def habit() -> Group:
     pass
 
+
+@habit.command(name='create')
+def habit_create() -> Command:
+    pass
+
+
+@habit.command(name='delete')
+def habit_delete() -> Command:
+    pass
+
+
+@habit.command(name='modify')
+def habit_modify() -> Command:
+    pass
+
+
+@habit.command(name='complete')
+def habit_complete() -> Command:
+    pass
