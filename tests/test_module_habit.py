4521efc54ab7b931182e05228b07a766f2b549a9
Jannik Weise <mail@jannikweise.de>
2023-10-08 01:29:48 +0200

    Improved Habit Module Test Imports

--- tests/test_module_habit.py
+++ tests/test_module_habit.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from classes.orm.base import Base
-from modules import habit
+from modules.habit import habit
 
 # Set up the SQLAlchemy session
 engine = create_engine('sqlite:///:memory:', echo=True)
@@ -21,20 +21,20 @@ def runner():
 
 
 def test_create(runner):
-    result = runner.invoke(habit.habit, ['create'], obj={'session_maker': session_maker}, input='Test Habit\nd\n')
+    result = runner.invoke(habit, ['create'], obj={'session_maker': session_maker}, input='Test Habit\nd\n')
     assert 'Habit "Test Habit" has been created with a Daily Periodicity!' in result.output
 
 
 def test_complete(runner):
-    result = runner.invoke(habit.habit, ['complete'], obj={'session_maker': session_maker}, input='Test Habit')
+    result = runner.invoke(habit, ['complete'], obj={'session_maker': session_maker}, input='Test Habit')
     assert 'You have completed Habit "Test Habit"! (Streak: 1)' in result.output
 
 
 def test_modify(runner):
-    result = runner.invoke(habit.habit, ['modify'], obj={'session_maker': session_maker}, input='1\nY\nChanged Habit\nN')
+    result = runner.invoke(habit, ['modify'], obj={'session_maker': session_maker}, input='1\nY\nChanged Habit\nN')
     assert 'Habit has been updated!' in result.output
 
 
 def test_delete(runner):
-    result = runner.invoke(habit.habit, ['delete'], obj={'session_maker': session_maker}, input='Changed Habit\nY')
+    result = runner.invoke(habit, ['delete'], obj={'session_maker': session_maker}, input='Changed Habit\nY')
     assert 'Habit "Changed Habit" has been deleted!' in result.output
