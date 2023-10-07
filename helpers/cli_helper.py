317d6de6c7167e2a3601956ebfcb04d8c816f6cb
Jannik Weise <mail@jannikweise.de>
2023-10-07 05:01:40 +0200

    Analytics Module Functionality
    Functionality has been added for the following commands:
    - analytics list
    - analytics streak
    
    analytics list allows the User to view more detailed data about their existing Habits.
    Habits can be filtered by Periodicity and sorted by various fields.
    Whilst the base "habit" command already shows a list of existing habits, the analytics variant shows some additional data.
    
    analytics streak shows the longest (currently active) streak across all Habits.
    Additionally, it can show the data for a specific Habit by passing its ID / Name.
    
    ---
    
    list_habits in cli_helper now supports extra headers.
    Additionally, if a List of Rows is passed instead of a List of Habits, all fields in the Row are added to the Table Data.

--- helpers/cli_helper.py
+++ helpers/cli_helper.py
@@ -1,23 +1,37 @@
-from typing import Type, List, Optional
+from typing import Type, List, Optional, Tuple
 
 import click
+from sqlalchemy import Row
 from tabulate import tabulate
 
 from classes.helpers.terminal_options import TerminalColor, TerminalFormat
 from classes.orm.habit import Habit
 
 
-def list_habits(habits: List[Type[Habit]]) -> None:
+def list_habits(habits: List[Type[Habit] | Row], extra_headers: Optional[List[str]] = None) -> None:
     """
     Prints a formatted table of the given list of habits.
 
     :param habits: List of Habits to be printed
+    :param extra_headers: List of extra headers to be added to the table
     """
     data = []
     headers = ["ID", "Name", "Current Streak", "Longest Streak", "Periodicity"]
+    if extra_headers is not None:
+        headers.extend(extra_headers)
 
-    for habit in habits:
-        data.append([habit.habit_id, habit.name, habit.streak, habit.highest_streak, habit.periodicity.name])
+    if isinstance(habits[0], Habit):
+        for habit in habits:
+            data.append([habit.habit_id, habit.name, habit.streak, habit.highest_streak, habit.periodicity.name])
+    elif isinstance(habits[0], Row):
+        for habit in habits:
+            habit_data = []
+            for field in habit:
+                if isinstance(field, Habit):
+                    habit_data.extend([field.habit_id, field.name, field.streak, field.highest_streak, field.periodicity.name])
+                else:
+                    habit_data.append(field)
+            data.append(habit_data)
 
     print(tabulate(tabular_data=data, headers=headers))
 
--- modules/analytics.py
+++ modules/analytics.py
@@ -1,9 +1,15 @@
-from typing import Optional
+from typing import Optional, Tuple
 
 import click
 from click import Group, Context
+from sqlalchemy import func
+from sqlalchemy.orm import Session, Mapped
 
+from classes.helpers.terminal_options import TerminalColor
+from classes.orm.habit import Habit
+from classes.orm.habit_entry import HabitEntry
 from classes.periodicity import Periodicity
+from helpers.cli_helper import colored_print, list_habits
 from helpers.validations import validate_periodicity
 
 
@@ -17,22 +23,48 @@ def analytics() -> Group:
 
 @analytics.command(name='list')
 @click.option('-p', '--period', 'periodicity', default=None, help='Periodicity of the Habit(s) that should be searched for.', type=click.UNPROCESSED, callback=validate_periodicity)
+@click.option('-s', '--sort', 'sort_order', default='CreationDate', help='Field by which Habit(s) should be sorted by.', type=click.Choice(['ID', 'Name', 'Streak', 'HighestStreak', 'Periodicity', 'CreationDate', 'TotalCompletions', 'MostRecentCompletion'], case_sensitive=False))
+@click.option('--asc', 'sort_order_asc', default=False, is_flag=True, help='Sort Habit(s) in ascending order.', type=bool)
+@click.option('--desc', 'sort_order_desc', default=False, is_flag=True, help='Sort Habit(s) in descending order.', type=bool)
 @click.pass_context
-def analytics_list(ctx: Context, periodicity: Optional[Periodicity]) -> None:
+def analytics_list(ctx: Context, periodicity: Optional[Periodicity], sort_order: str, sort_order_asc: bool, sort_order_desc: bool) -> None:
     """\b
     Lists all existing Habits.
+
     If a Periodicity is given, uses it as a filter.
+    If a Sort Order is given, uses it to sort the Habits by the given field.
     """
-    pass
+    with Session(ctx.obj['connection']) as session:
+        query = (session.query(Habit,
+                               func.count(HabitEntry.habit_entry_id).label('total_completions'),
+                               func.max(HabitEntry.completion_date).label('most_recent_completion'))
+                        .join(HabitEntry, Habit.habit_id == HabitEntry.habit_id, isouter=True)
+                        .group_by(Habit.habit_id))
+
+        if periodicity is not None:
+            query = query.filter(Habit.periodicity == periodicity)
+
+        target, number_based = get_sort_target(sort_order)
+        if number_based and not sort_order_asc and not sort_order_desc:
+            sort_order_desc = True
+
+        if target is not None:
+            if sort_order_desc:
+                query = query.order_by(target.desc())
+            else:
+                query = query.order_by(target.asc())
+
+        habits = query.all()
+        list_habits(habits, extra_headers=['Total Completions', 'Most Recent Completion'])
 
 
 @analytics.command(name='streak')
 @click.option('-i', '--id', 'habit_id', type=int, default=None, help='ID of the Habit that should be searched for. Takes precedence over --name.')
 @click.option('-n', '--name', type=str, default=None, help='Name of the Habit that should be searched for.')
 @click.option('-p', '--period', 'periodicity', default=None, help='Periodicity of the Habit(s) that should be searched for.', type=click.UNPROCESSED, callback=validate_periodicity)
-@click.option('-a', '--active', help='Get the longest streak that is currently active', type=bool)
+@click.option('-a', '--active', default=False, is_flag=True, help='Get the longest streak that is currently active', type=bool)
 @click.pass_context
-def analytics_longest_streak(ctx: Context, habit_id: Optional[int], name: Optional[str], periodicity: Optional[Periodicity]) -> None:
+def analytics_longest_streak(ctx: Context, habit_id: Optional[int], name: Optional[str], periodicity: Optional[Periodicity], active: bool) -> None:
     """\b
     Find the longest streak of a Habit based on the given parameters.
     Habits are identified either by their ID or their Name.
@@ -40,4 +72,73 @@ def analytics_longest_streak(ctx: Context, habit_id: Optional[int], name: Option
     If neither an ID nor a Name is given, the Habit with the longest total streak is returned.
     This can additionally be refined by specifying a Periodicity.
     """
-    pass
+    with Session(ctx.obj['connection']) as session:
+        query = session.query(Habit)
+
+        specific_habit = habit_id is not None or name is not None
+        if habit_id is not None:
+            query = query.filter(Habit.habit_id == habit_id)
+        elif name is not None:
+            query = query.filter(Habit.name == name)
+
+        if periodicity is not None:
+            query = query.filter(Habit.periodicity == periodicity)
+
+        sort_order = Habit.streak if active else Habit.highest_streak
+        query = query.order_by(sort_order.desc())
+
+        habit_with_longest_streak = query.first()
+
+        if habit_with_longest_streak is None:
+            colored_print('No Matching Habit found.', TerminalColor.YELLOW)
+            return
+
+        streak_value = habit_with_longest_streak.streak if active else habit_with_longest_streak.highest_streak
+
+        if specific_habit:
+            colored_print(f"The Habit {habit_with_longest_streak.name} has a{'n active' if active else ' longest'} streak of {streak_value}", TerminalColor.GREEN)
+        else:
+            colored_print(f"The Habit with the longest{' active' if active else ''} streak is: {habit_with_longest_streak.name} with a streak of {streak_value}", TerminalColor.GREEN)
+
+
+# region Helpers
+
+def get_sort_target(sort: str) -> Tuple[Mapped, bool]:
+    """
+    Returns the SQLAlchemy Mapped Object that should be used for sorting and whether the target is number-based.
+    Number-based targets are sorted in descending order by default.
+
+    :param sort: Sort Order to be used
+    :returns Mapped: SQLAlchemy Mapped Object to be used for sorting
+    :returns bool: Whether the target is number-based
+    """
+    target: Mapped
+    number_based: bool = False
+
+    if sort == 'ID':
+        target = Habit.habit_id
+    elif sort == 'Name':
+        target = Habit.name
+    elif sort == 'Streak':
+        target = Habit.streak
+        number_based = True
+    elif sort == 'HighestStreak':
+        target = Habit.highest_streak
+        number_based = True
+    elif sort == 'Periodicity':
+        target = Habit.periodicity
+    elif sort == 'CreationDate':
+        target = Habit.creation_date
+        number_based = True
+    elif sort == 'TotalCompletions':
+        target = func.count(HabitEntry.habit_entry_id)
+        number_based = True
+    elif sort == 'MostRecentCompletion':
+        target = func.max(HabitEntry.completion_date)
+        number_based = True
+    else:
+        raise NotImplementedError(f'No implementation for sort order {sort}!')
+
+    return target, number_based
+
+# endregion
